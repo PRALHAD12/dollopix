b22d522c3e63a9eb616da98811aaf58d1eb9d097
Palhad <95328259+PRALHAD12@users.noreply.github.com>
2025-12-15 12:57:51 +0530

    Implement Auth

---
+++ src/utils/format.js
@@ -0,0 +1,12 @@
+export const formatValidationErrors = (errors) => {
+    if (!errors || !errors.issues ) return 'validation Failed';
+
+    if (Array.isArray(errors.issues)) {
+        return errors.issues.map(issue => ({
+            field: issue.path.join('.'),
+            message: issue.message
+        }));
+    }
+
+    return JSON.stringify(errors);
+}
---
+++ src/validations/auth.validations.js
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+
+export const signupSchema = z.object({
+    name: z.string().trim().min(2).max(100),
+    email: z.string().trim().toLowerCase().email(),
+    password: z.string().min(8).max(100),
+    role: z.enum(['user', 'admin']).default('user'),
+});
+
+export const signinSchema = z.object({
+    email: z.string().trim().toLowerCase().email(),
+    password: z.string().min(8),
+});
